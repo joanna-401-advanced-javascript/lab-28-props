3b39443c0ef8b5c9ca70247b46decbd73045f681
joannaarroyo <joliuac@gmail.com>
2019-09-18 13:12:51 -0700

    added new message component

--- src/index.js
+++ src/index.js
@@ -3,9 +3,28 @@ import ReactDOM from 'react-dom';
 
 import './styles.css';
 
+class Message extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      text: 'Hello World',
+    };
+  }
+
+  render(){
+    return (
+      <div>{this.state.text}</div>
+    )
+  }
+}
+
 class App extends React.Component {
   render() {
-    return <div />;
+    return (
+      <>
+        <Message />
+      </>
+    )
   }
 }
 
