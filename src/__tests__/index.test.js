61666f9ba794e84dead5da5b7f7e3e38e0c00a79
joannaarroyo <joliuac@gmail.com>
2019-09-18 13:15:17 -0700

    changed test, assignment 1 challenge 1 completed

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 // import renderer from 'react-test-renderer';
-import App from '../app';
+import App from '../index';
 
 describe('<App />', () => {
   it('is rendered at application start', () => {
     const app = shallow(<App />);
-    expect(app.find('h1').exists()).toBe(true);
+    expect(app.find('div').exists()).toBe(true);
   });
 });
